e98725f1bfccba3ff38a36b67d2dc23d54f5a838
Jack Anderson <whotfisjax@gmail.com>
2024-05-22 19:04:02 -0400

    v1

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
                 href="https://www.dextools.io/app/en/solana/pair-explorer/7y3KMHA91JEBoQ67P3mEJtVvXLg62FjWR1VDHhEFSX8Z?t=1715720852830"
                 className='font-custom text-4xl md:text-5xl lg:text-6xl bg-amber-400 p-6 rounded-xl border-2 border-black ring-4 ring-amber-600 ring-opacity-80 ring-offset-4 hover:scale-105 transition ease-in-out duration-200'
               >
-                BUY
+                BUY NOW
               </a>
               <div className='pt-8 text-center font-custom text-2xl flex justify-center -mt-3'>
                 <a href="https://www.dextools.io/app/en/solana/pair-explorer/7y3KMHA91JEBoQ67P3mEJtVvXLg62FjWR1VDHhEFSX8Z?t=1715720852830" className='p-2 hover:scale-110 transition ease-in-out duration-200'><img src={DT} alt='Xlogo' className='w-12 h-12 rounded-md'></img></a>
